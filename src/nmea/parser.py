5ceff328a9d5c2764e64b0340657858a8cff115e
Alex Zalo <zalo.alex56@gmail.com>
2024-06-12 17:28:12 +0200

    sentence check

--- src/nmea/parser.py
+++ src/nmea/parser.py
@@ -10,24 +10,39 @@ class IIMWV(ParsedSentence):
         self.wind_direction = float(values[1])
         self.wind_speed = float(values[3])
         self.state = values[5]
-
-class WIXDR(ParsedSentence):
     
-    SENTENCE_TYPE = "WIXDR"
+    def __str__(self) -> str:
+        return f"WD={self.wind_direction} WS={p.wind_speed} S={p.state}"
     
-    def __init__(self, values):
-        self.temp = float(values[2])
+    @staticmethod
+    def check(values):
+        return not any([
+            values[0] != "$IIMWV",
+            values[2] != "R",
+            values[4] != "N",
+            values[5] not in "AV",
+        ])
 
 def get_parsed(sentence):
     values = sentence[:-3].split(",")
     sentence_type = values[0]
     
     types = {
-        "$IIMWV": IIMWV,
-        "$WIXDR": WIXDR
+        "$IIMWV": IIMWV
     }
     
-    if sentence_type in types:
+    if sentence_type in types and types[sentence_type].check(values):
         return types[sentence_type](values)
     else:
         return None
+
+# TESTS
+if __name__ == "__main__":
+    sentence = "$IIMWV,237.0,R,002.4,N,A*3D"
+    print(sentence)
+    p = get_parsed(sentence)
+    print(p)
+    sentence = "$IIMWV,053.0,$IIMWV,062.0,R,002.7,N,A*3C"
+    print(sentence)
+    p = get_parsed(sentence)
+    print(p)
